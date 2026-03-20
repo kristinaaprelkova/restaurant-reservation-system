050c1210dd888cb88ccd78285f46f5df7fd98423
kristinaaprelkova <kristina.aprelkova@gmail.com>
2026-03-20 07:53:41 +0200

    Initial commit

---
+++ src/test/java/ee/kristina/restaurant_reservation/RestaurantReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kristina.restaurant_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
